604d098604d6582ec66015603fa755965a8e8dbc
Dave Hunt <dave.hunt@gmail.com>
2016-06-30 11:05:44 +0100

    Add classifiers for supported python versions

--- setup.py
+++ setup.py
@@ -24,4 +24,8 @@ setup(name='pytest-repeat',
           'Topic :: Utilities',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7'])
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5'])
