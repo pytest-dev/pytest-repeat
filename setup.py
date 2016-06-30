2d67c1bdde44aa645f685ab639e9f73a7f3f8807
Dave Hunt <dave.hunt@gmail.com>
2016-06-30 11:06:10 +0100

    Added pytest classifier to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(name='pytest-repeat',
       keywords='py.test pytest repeat',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
+          'Framework :: Pytest',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
           'Operating System :: POSIX',
