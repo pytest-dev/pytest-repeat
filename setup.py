661e2019f78911dfac1cd69854be02cdeb820c4e
Miguel Sánchez de León Peque <peque@neosit.es>
2018-07-12 16:36:59 +0200

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -25,9 +25,7 @@ setup(name='pytest-repeat',
           'Topic :: Software Development :: Testing',
           'Topic :: Utilities',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5'])
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'])
