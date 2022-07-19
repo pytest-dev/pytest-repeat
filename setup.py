4e47702e1fb5f4129dc49ac2a6ed018ea6baebdb
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-07-19 21:19:23 +0300

    Add support for Python 3.10-3.11

--- setup.py
+++ setup.py
@@ -30,4 +30,6 @@ setup(name='pytest-repeat',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
       ])
