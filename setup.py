5dd7b8556369fc793bcc3ca2936543d3f97f12c6
Brian Okken <1568356+okken@users.noreply.github.com>
2023-10-04 12:22:54 -0700

    py312

--- setup.py
+++ setup.py
@@ -32,4 +32,5 @@ setup(name='pytest-repeat',
           'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: 3.10',
           'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
       ])
