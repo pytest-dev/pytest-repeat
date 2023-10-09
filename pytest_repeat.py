9748d4c62ecdc497344088c6e3af1b0a9ab85de5
Brian Okken <1568356+okken@users.noreply.github.com>
2023-10-08 22:35:57 -0700

    remove some debug code

--- pytest_repeat.py
+++ pytest_repeat.py
@@ -34,9 +34,8 @@ class UnexpectedError(Exception):
     pass
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def __pytest_repeat_step_number(request):
-    print('in repeat fixture')
     marker = request.node.get_closest_marker("repeat")
     count = marker and marker.args[0] or request.config.option.count
     if count > 1:
@@ -54,7 +53,6 @@ def __pytest_repeat_step_number(request):
 
 @pytest.hookimpl(trylast=True)
 def pytest_generate_tests(metafunc):
-    print('in generate tests')
     count = metafunc.config.option.count
     m = metafunc.definition.get_closest_marker('repeat')
     if m is not None:
